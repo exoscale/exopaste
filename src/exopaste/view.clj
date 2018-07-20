a83970c3910f497081dccd8433005bb244054663
Chris Glass <chris@exoscale.ch>
2018-07-20 08:06:20 +0200

    Added meta-charset.

--- src/exopaste/view.clj
+++ src/exopaste/view.clj
@@ -5,20 +5,21 @@
 (defn render-paste
   "given a map representing a paste, return an HTML string for the display of a paste."
   [paste]
-  (html5 {:lang "en"}
-         [:head
+  (html5 [:head
           (include-js "https://sos-de-fra-1.exo.io/highlight.js/9.12.0/highlight.min.js")
           (include-js "https://sos-de-fra-1.exo.io/highlight.js/9.12.0/go.min.js")
           (include-js "https://sos-de-fra-1.exo.io/highlight.js/9.12.0/clojure.min.js")
           (include-js "https://sos-de-fra-1.exo.io/highlight.js/9.12.0/yaml.min.js")
           (include-css "https://sos-de-fra-1.exo.io/highlight.js/9.12.0/default.min.css")
+          [:meta {:charset "UTF-8"}]
           [:script "hljs.initHighlightingOnLoad();"]]
          [:body
           [:pre [:code (:content paste)]]]))
 
 (defn render-form
   []
-  (html5 {:lang "en"}
+  (html5 [:head
+          [:meta {:charset "UTF-8"}]]
          [:body
           (form-to [:post "/"]
                    (text-area {:cols 80
